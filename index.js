02de2ffe00b3af030464feea7283b1804085ef72
wtracy15 <117859071+wtracy15@users.noreply.github.com>
2025-09-16 21:52:02 -0400

    Feature complete, minimal styling

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
                     "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
                     "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?", "/"];
 
+let password1El = document.getElementById("password1")
+let password2El = document.getElementById("password2")
 
+function generatePasswords(){
+    let password1 = ""
+    let password2 = ""
+
+    for (let i = 0; i < 15; i++){
+        password1 += characters[Math.floor(Math.random() * characters.length)]
+        password2 += characters[Math.floor(Math.random() * characters.length)]
+    }
+
+    password1El.textContent = password1
+    password2El.textContent = password2
+}
 
 
